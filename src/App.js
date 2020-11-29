bace482dc15d84062ea9de294b7a56e251f7b71b
nirattasai <nirattasai.h@ku.th>
2020-11-29 15:52:26 +0700

    update index gui

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import Login from './components/login'
 function App() {
 
   function goLogin(){
-      
+
   }
 
   return (
     <div>
       <Navbar/>
-      <img src="/images/main.jpg" alt="main.jpg" width="100%" height="10%"/>
-      <p></p>
-      <body className="body">
-        <button type='button' className="button1" onClick={goLogin}>LOGIN</button>
-        <p></p>
-        <button type='button' className="button1">SUBMIT</button>
-      </body>
-      
+        <body className="body">
+          <button type='button' className="button1" onClick={goLogin}>LOGIN</button>
+          <p></p>
+          <button type='button' className="button1">SUBMIT</button>
+        </body>      
     </div>
   );
 }
