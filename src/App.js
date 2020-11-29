7b21dc25f3861709422f6bbba902009122d526db
nirattasai <nirattasai.h@ku.th>
2020-11-29 15:14:32 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Navbar from './Navbar'
 import Login from './components/login'
 function App() {
+
+  function goLogin(){
+      
+  }
+
   return (
     <div>
       <Navbar/>
       <img src="/images/main.jpg" alt="main.jpg" width="100%" height="10%"/>
       <p></p>
       <body className="body">
-        <button type='button' className="button1">LOGIN</button>
+        <button type='button' className="button1" onClick={goLogin}>LOGIN</button>
         <p></p>
         <button type='button' className="button1">SUBMIT</button>
       </body>
