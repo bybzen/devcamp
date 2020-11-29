66eaf82a982573d3e8a148fe295e8a12b216ace2
Thankorn Rattrakulthip <thanakorn.ratt@ku.th>
2020-11-29 15:24:07 +0700

    commit change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Navbar from './Navbar'
 import Login from './components/login'
+
+
 function App() {
   return (
     <div>
       <Navbar/>
-      <img src="./images/index.png" alt="index images"/>
+      
+      <img src="/images/index.png"  width="100%" alt="index images"/>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button><h1>Login</h1></button>
+      
+     
     </div>
+
   );
 }
 
