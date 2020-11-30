c4232af80a4da230952314329f09cffe12ba39cc
nirattasai <nirattasai.h@ku.th>
2020-11-30 14:44:21 +0700

    upload file

--- src/profile.js
+++ src/profile.js
@@ -1,23 +1,46 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { auth } from './firebase'
+import { auth, store } from './firebase'
+import Navbar from './Navbar'
 
 const Profile = () => {
     const history = useHistory()
+    const [file,setFile] = useState(null)
     useEffect(()=>{
         console.log(auth.currentUser)
-    }, [auth.currentUser])
+    }, [auth])
 
-function logout(){
-    auth.signOut()
-    history.replace('/login')
-}
-    return (
-        <div>
-            <button onClick={logout}>LOGOUT</button>
-            <input type="file" id="file_upload" />
-        </div>
-    )
+    function logout(){
+        auth.signOut()
+        history.replace('/login')
+    }
+
+    const Choose = event => {
+        const _file = event.target.files[0]
+        if(_file!==null){
+            setFile(_file)
+        }
+    }
+
+    async function Upload() {
+        const file_ref = store.ref(auth.currentUser.uid).child(file.name)
+        await file_ref.put(file)
+        const url = await file_ref.getDownloadURL()
+        console.log(url)
+    }
+
+
+
+
+
+        return (
+            <div>
+                <Navbar />
+                <button onClick={logout}>LOGOUT</button>
+                <input type="file" id="file_choose" onChange={Choose}/>
+                <button id="file_upload" onClick={Upload} >UPLOAD</button>
+            </div>
+        )
 }
 
-export default Profile
+ export default Profile
